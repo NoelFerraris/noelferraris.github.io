f484913fb7e99d2e92b7bdedbdf11eefbfbb37d2
Noel <nferrari@risd.edu>
2022-05-29 20:42:30 -0700

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ import './App.css';
 import { useState } from 'react';
 
 
-    /*
+
+
+
+function App() {
+
+  const [items, setItems] = useState(Array.from({ length: 20 }))
+
+  const fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      setItems(items.concat(Array.from({length : 20})))
+    }, 1500);
+  };
+
+  const style = {
+    height: 30,
+    border: "1px solid green",
+    margin: 6,
+    padding: 8
+  };
+
+
+  
+  return (
+    <div className="App">
+
+    <div>
+    <h1>demo: react-infinite-scroll-component</h1>
+    
     <InfiniteScroll
       dataLength={items.length}
       next={fetchMoreData}
@@ -17,19 +46,7 @@ import { useState } from 'react';
           div - #{index}
         </div>
       ))}
-    </InfiniteScroll>*/
-
-function App() {
-  const [items, setItems] = useState(["bla", "bla"])
-
-
-  
-  return (
-    <div className="App">
-
-    <div>
-    <h1>demo: react-infinite-scroll-component</h1>
-
+    </InfiniteScroll>
   </div>
 
 
