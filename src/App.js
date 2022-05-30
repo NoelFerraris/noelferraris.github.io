6d2a2da882f3e4af6ec992de3140811f48f5a3b8
Noel <nferrari@risd.edu>
2022-05-29 20:10:48 -0700

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import logo from './logo.svg';
 import { Canvas, useFrame } from '@react-three/fiber'
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState(["bla", "bla"])
   return (
     <div className="App">
 
-      <p>change</p>
+    <InfiniteScroll
+    dataLength={items.length} //This is important field to render the next data
+    //next={fetchData}
+    hasMore={true}
+    loader={<h4>Loading...</h4>}
+    endMessage={
+      <p style={{ textAlign: 'center' }}>
+        <b>Yay! You have seen it all</b>
+      </p>
+    }
+    // below props only if you need pull down functionality
+    refreshFunction={this.refresh}
+    pullDownToRefresh
+    pullDownToRefreshThreshold={50}
+    pullDownToRefreshContent={
+      <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+    }
+    releaseToRefreshContent={
+      <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+    }
+  >
+    {items}
+  </InfiniteScroll>
 
     </div>
   );
